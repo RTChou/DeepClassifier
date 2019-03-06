bf20b74d803add41e708d2f412be7806634464de
reneechou123 <reneechou123@gmail.com>
2019-03-06 11:26:07 -0500

    Update similarity_callback.py train.py

--- methods/similarity_callback.py
+++ methods/similarity_callback.py
@@ -1,9 +1,14 @@
 import numpy as np
 
-def run_sim(valid_smp, trainX, train_smp, txt_labels, top):
+def get_similarity(valid_smp, trainX, train_smp, txt_labels, top):
     for i in range(len(valid_smp)):
         sample = train_smp[valid_smp[i].item()] # sample name
         label = txt_labels[valid_smp[i].item()] # label
+        
+        sim = []
+        for j in range(trainX.shape[0]):
+            out = validation_model.predict_on_batch([trainX[valid_smp[i].item()], trainX[j]])
+            sim.append(out)
 
 
 def get_sim(smp_ind, trainX):
--- train.py
+++ train.py
@@ -1,13 +1,12 @@
 # import matplotlib
 # matplotlib.use('Agg')
 
-import argparse
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import scale
+import argparse
+from methods.utils import load_data, graph_embed
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
-from sklearn.neighbors import NearestNeighbors
 from methods.negative_sampling import sample_context_dist
 from methods.graphSemiCNN import GraphSemiCNN
 from sklearn.metrics import classification_report
@@ -24,32 +23,43 @@ def main():
     parser.add_argument('-e', '--exp', required=True, help='path to input gene expression data, with genes in rows and samples in columns')
     parser.add_argument('-l', '--label', required=True, help='path to input labels')
     parser.add_argument('-m', '--model', required=True, help='path to output trained model')
-    parser.add_argument('-b', '--label-bin', required=True, help='path to output label binarizer')
+    parser.add_argument('-b', '--label_bin', required=True, help='path to output label binarizer')
     parser.add_argement('-p', '--plot', required=True, help='path to output accuracy/loss plot')
     # parser.add_argement('-t', '--epochs', default=75, type=int, help='number of epochs to train for')
     args = parser.parse_args()
 
-    print('[INFO] loading training data...')
-    data = [] # training data
-    labels = []
+    # params
+    exp_path = args.exp
+    label_path = args.label
+    model_path = args.model
+    label_bin_path = args.label_bin
+    plot_path = args.plot
     
-    # load data, shuffle the samples, and scale data
-    exp_dst = pd.read_csv(args.exp, sep='\t', index_col=0)
-    label_dst = pd.read_csv(args.label, sep='\t', index_col=0)
-    
-    exp_dst = exp_dst.sample(frac=1, random_state=33, axis=1)
-    exp_dst = pd.DataFrame(scale(exp_dst), index=exp_dst.index, columns=exp_dst.columns)
-    label_dst = label_dst.replace(np.nan, 'unlabeled', regex=True)
+    nb_neighbors = 2
+    sample_size = 10000
 
-    # store data in list
-    for i in range(exp_dst.shape[1]):
-       exp = list(exp_dst.iloc[:,i].values)
-       label = label_dst.loc[[exp_dst.columns[i]]]['tissue'].item()
-       data.append([[i] for i in exp])
-       labels.append([label])
+    # load data, shuffle the samples, and scale data
+    print('[INFO] loading training data...')
+    data, labels = load_data(exp_path, label_path)
+
+    # convert training data to graph embedding
+    print('[INFO] creating KNN graph from training data...')
+    graph = graph_embed(data, nb_neighbors)
+
+    # sample context distribution for training
+    print('[INFO] sampling from graph and label context...')
+    input1_ind, input2_ind, output2 = sample_training_set(sample_size, graph, labels)
     
-    data = np.array(data)
-    labels = pd.DataFrame(labels)
+    
+    np.random.seed(123)
+    input1_ind = []
+    input2_ind = []
+    output2 = []
+    for i in range(sample_size):
+        sample = sample_context_dist(graph, txt_labels, 0.5, 0.5, 20, 2)
+        input1_ind.append(sample[0])
+        input2_ind.append(sample[1])
+        output2.append(sample[3])
 
     # split the data into training and test sets
     (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=33)
@@ -63,32 +73,6 @@ def main():
     testY = lb.transform(testY)
     nb_classes = len(lb.classes_)
 
-    print('[INFO] creating KNN graph from training data...')
-    # convert trainX to graph embedding
-    flat_list = []
-    for i in range(trainX.shape[0]):
-        sample = []
-        for j in range(trainX.shape[1]):
-            sample.append(trainX[i,j].item())
-        flat_list.append(sample)
-    
-    nbrs = NearestNeighbors(n_neighbors=1000, algorithm='ball_tree').fit(flat_list)
-    graph = nbrs.kneighbors_graph(flat_list, mode='distance').toarray()
-
-    print('[INFO] sampling from graph and label context...')
-    np.random.seed(123)
-
-    # sample context distribution for training
-    sample_size = 2000
-    input1_ind = []
-    input2_ind = []
-    output2 = []
-    for i in range(sample_size):
-        sample = sample_context_dist(graph, txt_labels, 0.5, 0.5, 20, 2)
-        input1_ind.append(sample[0])
-        input2_ind.append(sample[1])
-        output2.append(sample[3])
-    
     # sample validation examples
     valid_size = 5
     valid_smp = np.random.randint(trainX.shape[0], size=valid_size)
