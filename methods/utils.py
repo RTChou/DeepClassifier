79df60d6af2e656dfbd83dfc67d20bf1e3248132
reneechou123 <reneechou123@gmail.com>
2019-03-15 12:02:41 -0400

    Update callbacks.py utils.py

--- methods/utils.py
+++ methods/utils.py
@@ -102,6 +102,37 @@ def split_data(smp_names, inputs, outputs, portion=[.6, .2], random_seed=33):
     return trn, val, tst
 
 
+def similarity_callback(smp_val, dat, val_model, top=10, random_seed=308):
+    """
+        smp_val: validatation datasets from split_data()
+            dat: original input data, including sample names, expression data, and labels
+            top: number of nearest samples
+    random_seed: seed for random number generator
+    """
+    log_str = ''
+    for i in range(valid_size):
+        sim = []
+        target = smp_val['inp'][i]
+        for j in range(len(dat['smp'])):
+            context = dat['inp'][j]
+            out = val_model.predict_on_batch([target, context])
+            sim.append(out[1])
+
+        nearest = (-sim).argsort()[1:top + 1]
+        smp_name = smp_val['smp'][i]
+        label = smp_val['out'][i]
+        nst_smp_names = dat['smp'][nearest]
+        nst_labels = dat['out'][nearest]
+        log_str = 'Nearest to %s (%s): ' % (smp_name, label)
+
+        for k in range(top):
+            log_str = log_str + '%s (%s)' % (nst_smp_names[k], nst_labels[k])
+
+        log_str = log_str + '\n'
+
+    return log_str
+
+
 def plot_loss_acc(plot_path, nb_epochs, history):
     N = np.arange(0, nb_epochs)
     plt.style.use('ggplot')
