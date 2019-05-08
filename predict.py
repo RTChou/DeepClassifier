65dcd0e849a2f6e91f003c20fda8c31d03a244b9
reneechou123 <reneechou123@gmail.com>
2019-05-08 11:30:58 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,8 +5,8 @@ import pickle
 def main():
     parser = argparse.ArgumentParser(description="This script is for predicting using the trained semi-supervised neural network model")
     parser.add_argument('-e', '--exp', required=True, help='path to input gene expression data, with genes in rows and samples in columns')
-    parser.add_argument('-m', '--model', required=True, help='path to input trained model')
-    parser.add_argument('-b', '--label_bin', required=True, help='path to input label binarizer')
+    parser.add_argument('-m', '--model', required=True, help='path to trained model')
+    parser.add_argument('-b', '--label_bin', required=True, help='path to label binarizer')
 
     args = parser.parse_args()
 
